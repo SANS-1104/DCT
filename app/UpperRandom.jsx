4069dfdad785c9c5d9f8d7406835157f9b622f96
SANS-1104 <sanskriti020803@gmail.com>
2024-07-31 11:29:10 +0530

    commit 5

---
+++ app/UpperRandom.jsx
@@ -0,0 +1,15 @@
+import FeaturesSectionDemo from "./GlobeNew";
+
+export default function UpperRandom(){
+    return(
+        <div className="flex flex-wrap justify-between items-center">
+            <div className="content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic possimus ipsam ullam voluptates animi itaque quae, minus sed iste, architecto, ad explicabo a pariatur! Beatae sapiente delectus quas ad quis.
+            </div>
+            <div className="globe-display">
+                <FeaturesSectionDemo />
+            </div>
+            
+        </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -2,8 +2,10 @@ import Navbar from "./Navbar";
 import VortexDemo from "./HeroSection";
 import BackgroundBoxesDemo from "./AboutSection";
 import ServicesPage from "./ServicesPage";
+import FeaturesSectionDemo from "./GlobeNew";
 import RandomSection from "./RandomSection";
 import ShootingStarsAndStarsBackgroundDemo from "./Footer";
+import './css/UpperRandom.css'
 
 export default function Home() {
   return (
@@ -16,8 +18,11 @@ export default function Home() {
       <BackgroundBoxesDemo />
       </div>
       <ServicesPage />
+      <div className="content w-1/2 inline-block upper-random-bg px-8">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic possimus ipsam ullam voluptates animi itaque quae, minus sed iste, architecto, ad explicabo a pariatur! Beatae sapiente delectus quas ad quis.
+      </div>
+      <FeaturesSectionDemo />
       <RandomSection />  
-
       <ShootingStarsAndStarsBackgroundDemo />
       
     </div>
